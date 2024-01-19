b12032dfec412448d60519d974b1ae5e9a09fd2c
Ubinquitous <ubinquitous1@gmail.com>
2024-01-19 16:49:26 +0900

    logic

---
+++ index.js
@@ -0,0 +1,69 @@
+"use strict";
+exports.__esModule = true;
+exports.particle = exports.조사 = void 0;
+exports.조사 = {
+    은_는: "은_는",
+    이_야: "이_야",
+    이_여: "이_여",
+    이_라: "이_라",
+    으_로: "으_로",
+    와_과: "와_과",
+    을_를: "을_를",
+    이_가: "이_가",
+    이_랑: "이_랑"
+};
+var isUnderlay = function (word) {
+    if (typeof word !== "string")
+        return false;
+    var lastLetter = word[word.length - 1];
+    var uni = lastLetter.charCodeAt(0);
+    if (uni < 44032 || uni > 55203)
+        return false;
+    return (uni - 44032) % 28 != 0;
+};
+var particleSwitch = function (keyword, state, isName) {
+    switch (state) {
+        case exports.조사.은_는:
+            if (isUnderlay(keyword))
+                return isName ? "".concat(keyword, "\uC774\uB294") : "".concat(keyword, "\uC740");
+            return "".concat(keyword, "\uB294");
+        case exports.조사.이_야:
+            if (isUnderlay(keyword))
+                return "".concat(keyword, "\uC774\uC57C");
+            return "".concat(keyword, "\uC57C");
+        case exports.조사.이_여:
+            if (isUnderlay(keyword))
+                return "".concat(keyword, "\uC774\uC5EC");
+            return "".concat(keyword, "\uC5EC");
+        case exports.조사.이_라:
+            if (isUnderlay(keyword))
+                return "".concat(keyword, "\uC774\uB77C");
+            return "".concat(keyword, "\uB77C");
+        case exports.조사.으_로:
+            if (isUnderlay(keyword))
+                return isName ? "".concat(keyword, "\uC774\uB85C") : "".concat(keyword, "\uC73C\uB85C");
+            return "".concat(keyword, "\uB85C");
+        case exports.조사.와_과:
+            if (isUnderlay(keyword))
+                return isName ? "".concat(keyword, "\uC774\uC640") : "".concat(keyword, "\uACFC");
+            return "".concat(keyword, "\uC640");
+        case exports.조사.을_를:
+            if (isUnderlay(keyword))
+                return isName ? "".concat(keyword, "\uC774\uB97C") : "".concat(keyword, "\uC744");
+            return "".concat(keyword, "\uB97C");
+        case exports.조사.이_가:
+            if (isUnderlay(keyword))
+                return isName ? "".concat(keyword, "\uC774\uAC00") : "".concat(keyword, "\uC774");
+            return "".concat(keyword, "\uAC00");
+        case exports.조사.이_랑:
+            if (isUnderlay(keyword))
+                return "".concat(keyword, "\uC774\uB791");
+            return "".concat(keyword, "\uB791");
+    }
+};
+var particle = function (keyword) {
+    var name = function (state) { return particleSwitch(keyword, state, true); };
+    var word = function (state) { return particleSwitch(keyword, state, false); };
+    return { name: name, word: word };
+};
+exports.particle = particle;
---
+++ index.ts
@@ -0,0 +1,64 @@
+export const 조사 = {
+  은_는: "은_는",
+  이_야: "이_야",
+  이_여: "이_여",
+  이_라: "이_라",
+  으_로: "으_로",
+  와_과: "와_과",
+  을_를: "을_를",
+  이_가: "이_가",
+  이_랑: "이_랑",
+} as const;
+
+const isUnderlay = (word: string) => {
+  if (typeof word !== "string") return false;
+  const lastLetter = word[word.length - 1];
+  const uni = lastLetter.charCodeAt(0);
+  if (uni < 44032 || uni > 55203) return false;
+  return (uni - 44032) % 28 != 0;
+};
+
+const particleSwitch = (keyword: string, state: string, isName: boolean) => {
+  switch (state) {
+    case 조사.은_는:
+      if (isUnderlay(keyword))
+        return isName ? `${keyword}이는` : `${keyword}은`;
+      return `${keyword}는`;
+    case 조사.이_야:
+      if (isUnderlay(keyword)) return `${keyword}이야`;
+      return `${keyword}야`;
+    case 조사.이_여:
+      if (isUnderlay(keyword)) return `${keyword}이여`;
+      return `${keyword}여`;
+    case 조사.이_라:
+      if (isUnderlay(keyword)) return `${keyword}이라`;
+      return `${keyword}라`;
+    case 조사.으_로:
+      if (isUnderlay(keyword))
+        return isName ? `${keyword}이로` : `${keyword}으로`;
+      return `${keyword}로`;
+    case 조사.와_과:
+      if (isUnderlay(keyword))
+        return isName ? `${keyword}이와` : `${keyword}과`;
+      return `${keyword}와`;
+    case 조사.을_를:
+      if (isUnderlay(keyword))
+        return isName ? `${keyword}이를` : `${keyword}을`;
+      return `${keyword}를`;
+    case 조사.이_가:
+      if (isUnderlay(keyword))
+        return isName ? `${keyword}이가` : `${keyword}이`;
+      return `${keyword}가`;
+    case 조사.이_랑:
+      if (isUnderlay(keyword)) return `${keyword}이랑`;
+      return `${keyword}랑`;
+  }
+};
+
+export const particle = (keyword: string) => {
+  const name = (state: string) => particleSwitch(keyword, state, true);
+
+  const word = (state: string) => particleSwitch(keyword, state, false);
+
+  return { name, word };
+};
